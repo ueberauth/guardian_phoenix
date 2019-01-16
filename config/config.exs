e653c535d1513d888b6518a8851a471a86ff33a2
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-15 19:39:34 -0800

    Add phoenix config

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,5 @@
 use Mix.Config
+
+config :guardian, Guardian.Phoenix.ControllerTest.Endpoint, secret_key_base: "lksdjfl"
+config :guardian, Guardian.Phoenix.SocketTest.Impl, []
+config :guardian, Guardian.Phoenix.Permissions.BitwiseTest.Impl, []
