11dd5d7f70db9421c92df055b42b56164700b5a4
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2019-01-15 19:39:26 -0800

    Add phoenix deps and fix elixir path

--- mix.exs
+++ mix.exs
@@ -47,8 +47,13 @@ defmodule GuardianPhoenix.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   defp deps do
     [
+      {:phoenix, "~> 1.3"},
+
       # Tools
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, ">= 1.0.0-rc4", only: [:dev], runtime: false},
