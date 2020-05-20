e66b046302f8c18d51bbbbb6186a13db5c0402c2
Hans <hanspagh@gmail.com>
2020-05-20 10:23:31 +0200

    Upgrade to v1.5 (#8)
    * upgrade to phoenix 1.5
    
    * start endpoint, PubSub and Presence as part of the tests
    
    * run formatter
    
    Co-authored-by: Michael Agra <michael.agra@yarilabs.com>
    Co-authored-by: Hans Pagh <hans.pagh@maersk.com>

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,11 @@
 ExUnit.start()
+
+opts = [strategy: :one_for_one, name: Controller.Supervisor]
+
+children = [
+  Guardian.Phoenix.TestSupport.Endpoint,
+  {Phoenix.PubSub, name: Guardian.Phoenix.PubSub},
+  Guardian.Phoenix.TestSupport.Presence
+]
+
+Supervisor.start_link(children, opts)
